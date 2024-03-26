e7b2e2d33d07e4284a18786a4e4b5c6e1aff444e
elme4147 <64235489+elimedina247@users.noreply.github.com>
2024-03-26 14:00:49 -0600

    Setting up routing foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
+
 
 //May not need this file it may all be in app.component.ts now?
 
 
 @NgModule({
-  declarations: [AppComponent,
-    HeaderComponent,NavComponent],
+  declarations: [],
   imports: [
     CommonModule,
     BrowserModule
   ],
-  bootstrap: [AppComponent,HeaderComponent,NavComponent]
+  bootstrap: []
 })
 export class AppModule { }
